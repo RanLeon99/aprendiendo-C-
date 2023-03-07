945458e8668d21f48ccaae8b5df517759306f5f9
Randall Leon <randall.perico@gmail.com>
2023-03-07 14:12:11 -0600

    IMPRIMIR EN PANTALLA Y RECIBIR PARAMETROS

---
+++ hola-mundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main () {
+
+	cout<<"HOLA MUNDO";
+
+	
+	return 0;
+}
---
+++ recibiendo-parametros.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+int main() {
+	
+	int numero1, numero2, suma;
+	
+	cout<<"BIENVENIDO";
+	
+	cout<<"/n DIGITE EL PRIMER NUMERO: ";
+	cin>>numero1;
+	
+	cout<<"/n DIGITE EL SEGUNDO NUMERO: ";
+	cin>>numero2;
+	
+	suma = numero1 +numero2;
+	
+	cout<<"LA SUMA ES:"<<suma;
+	
+	
+	return 0;
+}
