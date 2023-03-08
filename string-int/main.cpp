41d4e7abaa096de6ec8bd0f0dff8185f5f51e066
Randall Leon <randall.perico@gmail.com>
2023-03-08 10:44:48 -0600

    TIPOS DE VARIABLES C++

---
+++ string-int/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string nombre = "Randall";
+    int edad = 23;
+    string apellido = "Leon";
+
+
+    cout << "Mi nombre es " << nombre << apellido << endl;
+    cout << "Mi apellido es " << edad << endl;
+
+
+    return 0;
+}
+
---
+++ type-variables/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grado = 'A'; // Este tipo solo almacena un caracter
+    string texto = "Randal leon 2"; // este acrcater es una cadena
+    int edad = 23;
+    float altura = 1.87;
+    double pi = 3.1416954622616; // Este se usa cuando se necesite muchos decimales
+
+
+
+    cout << " Grado: " << grado << endl;
+    cout << " texto: " << texto << endl;
+    cout << " edad: " << edad << endl;
+    cout << " altura: " << altura << endl;
+    cout << " pi: " << pi << endl;
+
+
+    return 0;
+}
