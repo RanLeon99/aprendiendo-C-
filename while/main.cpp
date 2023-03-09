9da8c602773f657ba2b4bb097851a20088addc3f
Randall Leon <randall.perico@gmail.com>
2023-03-08 21:48:04 -0600

    BUCLE WHILE

---
+++ while/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int condition = 0 ;
+
+
+
+    while(condition <= 10){
+
+        cout<<condition<<endl;
+
+        condition += 1;
+
+    }
+    return 0;
+}
